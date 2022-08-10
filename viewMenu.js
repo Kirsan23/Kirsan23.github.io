0f7ed3a5f6feb0ada53be187b6b7bad366c6c048
Kirsan23 <36918043+Kirsan23@users.noreply.github.com>
2022-08-10 15:47:25 +0300

    New Commit

--- viewMenu.js
+++ viewMenu.js
@@ -1,16 +1,43 @@
 function viewMenu(){
     document.getElementById("menu_list").classList.toggle("show");
+    document.getElementById("menu_button").classList.toggle("icon-Icon-12");
+    document.getElementById("menu_button").classList.toggle("icon-Icon-11");
 };
 
 window.onclick = function(event) {
     if (!event.target.matches(".menu_button")) {
       var dropdowns = document.getElementsByClassName("menu_list_content");
+      var test = document.getElementsByClassName("menu_button");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
+        var test2 = test[i];
         if (openDropdown.classList.contains("show")) {
           openDropdown.classList.remove("show");
+        }
+        if (test2.classList.contains("icon-Icon-12")) {
+          test2.classList.remove("icon-Icon-12");
+          test2.classList.add("icon-Icon-11");
         }
       }
    }
 };
+
+// window.onclick = function(event) {
+//   if (!event.target.matches(".menu_button")) {
+//     var dropdowns = document.getElementsById("menu_list");
+//     var test = document.getElementsById("menu_button");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       var test2 = test[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//       if (test2.classList.contains("icon-Icon-12")) {
+//         test2.classList.remove("icon-Icon-12");
+//         test2.classList.add("icon-Icon-11");
+//       }
+//     }
+//  }
+// };
